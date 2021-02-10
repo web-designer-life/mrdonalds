ee36916f09f11ba5cf44449565c92e893b28b975
Anton Yulbarysau <io57196657@gmail.com>
2021-02-10 15:12:34 +0300

    add: deleteItem from Order

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -50,7 +50,13 @@ const EmptyList = styled.p`
     text-align: center;
 `;
 
-export const Order = ({ orders }) => {
+export const Order = ({ orders, setOrders }) => {
+    const deleteItem = (index) => {
+        const newOrders = orders.filter((item, i) => index !== i);
+
+        setOrders(newOrders);
+    };
+
     const total = orders.reduce((result, order) => totalPriceItems(order) + result, 0);
 
     const totalCounter = orders.reduce((result, order) => order.count + result, 0);
@@ -61,7 +67,12 @@ export const Order = ({ orders }) => {
             <OrderContent>
                 {orders.length ? 
                 <OrderList>
-                    {orders.map(order => <OrderListItem order={order}/>)}
+                    {orders.map((order, index) => <OrderListItem 
+                        key={index}
+                        order={order}
+                        deleteItem={deleteItem}
+                        index={index}
+                    />)}
                 </OrderList> : 
                 <EmptyList>Список заказов пуст</EmptyList>}
             </OrderContent>
--- src/Components/Order/OrderListItem.js
+++ src/Components/Order/OrderListItem.js
@@ -38,20 +38,18 @@ const Toppings = styled.div`
     width: 100%;
 `;
 
-export const OrderListItem = ({ order }) => {
+export const OrderListItem = ({ order, index, deleteItem }) => {
     const toppings = order.topping
         .filter((item) => item.checked)
         .map((item) => item.name)
         .join(', ');
-
-
 
     return (
         <OrderItemStyled>
             <ItemName>{order.name} {order.choice}</ItemName>
             <span>{order.count}</span>
             <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
-            <TrashButton/>
+            <TrashButton onClick={() => deleteItem(index)}/>
             {toppings && <Toppings>Допы: {toppings}</Toppings>}
         </OrderItemStyled>
     )
